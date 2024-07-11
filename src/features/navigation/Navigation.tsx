6977c76d7a3602cc8229d1cda6994e3c540482d1
root <root@Zhenya-PC>
2024-07-11 15:58:52 +0300

    Added hover to nav items

--- src/features/navigation/Navigation.tsx
+++ src/features/navigation/Navigation.tsx
@@ -1,43 +1,60 @@
-import { navigationData } from "@/features/navigation/data";
-import { NavigationItemData, NavigationItemLabels } from "@/features/navigation/types";
-import classNames from "classnames";
+import { navigationData } from '@/features/navigation/data';
+import {
+    NavigationItemData,
+    NavigationItemLabels,
+} from '@/features/navigation/types';
+import classNames from 'classnames';
+import { useState } from 'react';
 
 const NavigationItem = ({
-  icon,
-  activeIcon,
-  text,
-  isActive = false,
-}: Omit<NavigationItemData, "label">) => {
-  const itemClass = classNames({
-    "text-grayscale/40": !isActive,
-    "text-grayscale/70": isActive,
-  });
+    icon,
+    activeIcon,
+    text,
+    isActive = false,
+}: Omit<NavigationItemData, 'label'>) => {
+    const itemClass = classNames({
+        'text-grayscale/40': !isActive,
+        'text-grayscale/70': isActive,
+    });
 
-  return (
-    // заменить на Link
-    <div className="flex gap-[10px] items-center justify-center h-full cursor-pointer">
-      {isActive ? activeIcon : icon}
-      <p className={classNames(itemClass, "text-[14px] font-semibold")}>{text}</p>
-    </div>
-  );
+    const [isHovered, setIsHovered] = useState(false);
+    return (
+        // заменить на Link
+        <div
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            className="flex gap-[10px] items-center justify-center h-full cursor-pointer"
+        >
+            {isActive || isHovered ? activeIcon : icon}
+            <p
+                className={classNames(
+                    itemClass,
+                    isHovered && 'text-grayscale/70',
+                    'text-[14px] font-semibold transition',
+                )}
+            >
+                {text}
+            </p>
+        </div>
+    );
 };
 
 const Navigation = ({
-  activeItemLabel,
+    activeItemLabel,
 }: {
-  activeItemLabel: NavigationItemLabels;
+    activeItemLabel: NavigationItemLabels;
 }) => {
-  return (
-    <nav className="flex gap-[30px] items-center">
-      {navigationData.map((item) => (
-        <NavigationItem
-          key={item.label}
-          {...item}
-          isActive={activeItemLabel === item.label}
-        />
-      ))}
-    </nav>
-  );
+    return (
+        <nav className="flex gap-[30px] items-center">
+            {navigationData.map((item) => (
+                <NavigationItem
+                    key={item.label}
+                    {...item}
+                    isActive={activeItemLabel === item.label}
+                />
+            ))}
+        </nav>
+    );
 };
 
 export default Navigation;
